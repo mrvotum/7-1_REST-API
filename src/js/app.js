b984cf73204fd60c4e48fa2aeba03ecaa2bc5f2b
Vladimir <mr.qrsant@gmail.com>
2019-04-24 17:21:27 +0300

    чего-то добился

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,34 @@
 import API from './API.js';
 
-const api = new API('http://localhost:7075/contacts');
+const api = new API('http://localhost:7075/TicketFull');
 
 async function a() {
   {
-    const contacts = await api.load();
-    const data = await contacts.json();
+    const TicketFull = await api.load();
+    const data = await TicketFull.json();
     console.log(data);
   }
 
-  {
-    const contacts = await api.add({name: 'принтер', phone: '+8909111111'});
+  { // добавляем тикет
+    const TicketFull = await api.add({
+      id: 1,
+      name: 'принтер не работает',
+      description: 'кабинет 404, краска на складе 1Б',
+      status: false,
+      created: '12.01.1992',
+    });
+
+    // const Ticket = await api.add({
+    //   id: TicketFull[0],
+    //   name: TicketFull[2],
+    //   status: TicketFull[3],
+    //   created: TicketFull[4],
+    // });
   }
 
   {
-    const contacts = await api.load();
-    const data = await contacts.json();
+    const TicketFull = await api.load();
+    const data = await TicketFull.json();
     console.log(data);
   }
 }
